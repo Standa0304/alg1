c63991f0c474bd3f122e4630b14b00c4cd98a5b4
Standa0304 <63123347+Standa0304@users.noreply.github.com>
2024-04-11 15:39:30 +0200

    Update ConsoleApplication3.cpp

--- ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,144 +1,70 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <random>
+#include <algorithm>
+#include <limits>
 #include <chrono>
+#include <random>
 
-void selectionSort(std::vector<int>& pole) {
-    int min = 0;
-    int pom = 0;
+using namespace std;
 
-    for (size_t i = 0; i < pole.size() - 1; i++) {
-        min = pole[i];
-        for (size_t j = i + 1; j < pole.size(); j++) {
-            if (pole[j] < min) {
-                pom = pole[j];
-                pole[j] = min;
-                min = pom;
-            }
-            pole[i] = min;
+int assignmentProblem(const vector<vector<int>>& costMatrix) {
+    int n = costMatrix.size();
+    int minCost = numeric_limits<int>::max();
+    vector<int> assignment(n);
+
+    for (int i = 0; i < n; ++i) {
+        assignment[i] = i;
+    }
+    // todo loop over all assignments==permutations and find minimum 
+
+    while (next_permutation(assignment.begin(), assignment.end())); {
+        int cost = 0;
+        for (int i = 0; i < assignment.size(); ++i) {
+            cost += costMatrix[i][assignment[i]];
+        }
+        if (cost < minCost) {
+            minCost = cost;
         }
     }
+
+    return minCost;
 }
 
-void bubbleSort(std::vector<int>& pole) {
-    int pom2 = 0;
+vector<vector<int>> generateMatrix(int n, int minVal, int maxVal) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> distribution(minVal, maxVal);
 
-    for (size_t i = 0; i < pole.size() - 1; i++) {
-        for (size_t j = 0; j < pole.size() - 1; j++) {
-            if (pole[j] > pole[j + 1]) {
-                pom2 = pole[j];
-                pole[j] = pole[j + 1];
-                pole[j + 1] = pom2;
-            }
+    vector<vector<int>> result(n, vector<int>(n));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            result[i][j] = distribution(gen);
         }
-    }
-}
-
-
-void Leftto(std::vector<int>& pole, int& left, int& right) {
-    int j = 0;
-    for (int i = left; i < right - 1; i++) {
-        if (pole[i] > pole[i + 1]) {
-            std::swap(pole[i], pole[i + 1]);
-            j = i;
-        }
-    }
-    right = j;
-}
-
-void Rightto(std::vector<int>& pole, int& left, int& right) {
-    int j = 0;
-    for (int i = right; i >= left + 1; i--) {
-        if (pole[i - 1] > pole[i]) {
-            std::swap(pole[i - 1], pole[i]);
-            j = i;
-        }
-    }
-    left = j;
-}
-
-void shaker(std::vector<int>& pole) {
-    int left = 0;
-    int right = pole.size();
-
-    while (left <= right) {
-        Leftto(pole, left, right);
-        if (left >= right) break;
-        Rightto(pole, left, right);
-        if (left >= right) break;
-    }
-}
-
-std::vector<int> generate(int lenght, int minVal, int maxVal) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> distribution(minVal, maxVal);
-
-    std::vector<int> result;
-    result.reserve(lenght);
-
-    for (int i = 0; i < lenght; i++) {
-        result.push_back(distribution(gen));
     }
 
     return result;
-
 }
 
-int main() {
+int main()
+{
+    vector<vector<int>> testCostMatrix = {
+        {9, 2, 7, 8},
+        {6, 4, 3, 7},
+        {5, 8, 1, 8},
+        {7, 6, 9, 4}
+    };
 
-    std::vector<int> pole = generate(10, 0, 100);
-
-    std::cout << "před" << std::endl;
-    for (int i = 0; i < pole.size(); i++) {
-        std::cout << pole[i] << " ";
-    }
-
-    auto start = std::chrono::high_resolution_clock::now();
-    shaker(pole);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "Time: " << duration.count() << " sec" << std::endl;
-
-    std::cout << "po" << std::endl;
-    for (int i = 0; i < pole.size(); i++) {
-        std::cout << pole[i] << " ";
-    }
+    vector<vector<int>> bigCostMatrix = generateMatrix(13, 0, 100);
 
     auto star = std::chrono::high_resolution_clock::now();
-    bubbleSort(pole);
+    int vys = assignmentProblem(bigCostMatrix);
     auto en = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duratio = en - star;
     std::cout << "Time: " << duratio.count() << " sec" << std::endl;
 
-    auto sta = std::chrono::high_resolution_clock::now();
-    selectionSort(pole);
-    auto e = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> durati = e - sta;
-    std::cout << "Time: " << durati.count() << " sec" << std::endl;
+    cout << vys << std::endl;
 
     return 0;
+
 }
-
-
-// T(n) = 5T(n-1)
-// T(4) = 5T(3)
-// T(3) = 5*5T(2)
-// T(2) = 5*5*5T(1)
-// T(1) = 5*5*5*5T(0)
-// T(0) = 5*5*5*5*1
-
-//T(n) = 5^n
-
-
-/*    std::cout << "před" << std::endl;
-    for (int i = 0; i < pole.size(); i++) {
-        std::cout << pole[i] << " ";
-    }
-    shaker(pole);
-    std::cout << "po" << std::endl;
-    for (int i = 0; i < pole.size(); i++) {
-        std::cout << pole[i] << " ";
-    }
-    std::cout << "konec" << std::endl;*/
