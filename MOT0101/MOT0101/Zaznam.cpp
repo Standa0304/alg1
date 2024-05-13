e7fb1fd6d41c2dd706d5ba949ad3aaa666197e6c
Standa0304 <63123347+Standa0304@users.noreply.github.com>
2024-05-13 19:12:50 +0200

    Delete MOT0101 directory

--- MOT0101/MOT0101/Zaznam.cpp
+++
@@ -1,5 +0,0 @@
-#include "Zaznam.h"
-
-// Konstruktor tøídy Zaznam
-Zaznam::Zaznam(std::string tim, float napeti, float teplota, float vlhkost, Zn zn, std::string zaz)
-    : tim(tim), napeti(napeti), teplota(teplota), vlhkost(vlhkost), zn(zn), zaz(zaz) {}
